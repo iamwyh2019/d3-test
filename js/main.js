7c7460353d59fe449d6c5e72492664a0d026302c
yuhengwu <799810767@qq.com>
2021-10-23 19:02:13 +0800

    Add ticks & change font

--- js/main.js
+++ js/main.js
@@ -16,17 +16,19 @@ function draw_background() {
         .attr('height', SVG_HEIGHT)
         .attr('transform', `translate(${padding.left}, ${padding.top})`);
     
-    let title_y = SVG_HEIGHT * 0.1;
+    let title_y = SVG_HEIGHT * 0.08;
     let title = svg.append('text')
         .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'middle')
         .attr('id', 'title')
         .attr('x', SVG_WIDTH/2)
         .attr('y', title_y)
         .text('Transportation mode, by share of use');
 
-    let subtitle_y = title_y + title.node().getBBox().height/2;
+    let subtitle_y = title_y + title.node().getBBox().height/2+10;
     let subtitle = svg.append('text')
         .attr('text-anchor', 'end')
+        .attr('dominant-baseline', 'middle')
         .attr('id', 'subtitle')
         .attr('x', SVG_WIDTH/2)
         .attr('y', subtitle_y)
@@ -35,7 +37,31 @@ function draw_background() {
     let ticks = svg.append('g')
         .attr('id', 'ticks');
 
+    const tick_data = [
+        ['0%', BOTTOM_LINE_COLOR],
+        ['25%', TICK_LINE_COLOR],
+        ['50%', TICK_LINE_COLOR],
+        ['75%', TICK_LINE_COLOR]
+    ];
     
+    let tick_enter = ticks.selectAll('rect')
+        .data(tick_data)
+        .enter();
+    
+    tick_enter.append('rect')
+        .attr('x', (SVG_WIDTH - BAR_AREA_WIDTH)/2)
+        .attr('width', BAR_AREA_WIDTH)
+        .attr('y', (d,i) => (subtitle_y + BAR_AREA_HEIGHT * (1-i/tick_data.length)))
+        .attr('height', TICK_HEIGHT)
+        .attr('fill', d => d[1]);
+    
+    tick_enter.append('text')
+        .attr('text-anchor', 'end')
+        .attr('dominant-baseline', 'middle')
+        .attr('class', 'ticktext')
+        .attr('x', (SVG_WIDTH - BAR_AREA_WIDTH)/2 - 25)
+        .attr('y', (d,i) => (subtitle_y + BAR_AREA_HEIGHT * (1-i/tick_data.length) + TICK_HEIGHT/2))
+        .text(d => d[0]);
 }
 
 draw_background();
